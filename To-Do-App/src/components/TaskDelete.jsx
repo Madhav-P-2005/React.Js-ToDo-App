a015e7c0ac99484e8a714065f62cdd46fcaa870c
Madhav P <madhavp2023@gmail.com>
2025-07-22 05:29:37 +0530

    Level-8 Completed

--- To-Do-App/src/components/TaskDelete.jsx
+++ To-Do-App/src/components/TaskDelete.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { faT, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {faTrash} from "@fortawesome/free-solid-svg-icons";
 
 const TaskDelete = ({ taskId, onDelete }) => {
   const handleClick = () => {
